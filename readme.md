4f54386e38893681ec1923b5b3df353b002d8bdc
Egor Aristov <ef@efprojects.com>
2024-04-16 11:07:13 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# My utility function collection primarily for personal use
+
+## Install
+
+```shell
+go get pkg.efprojects.com/evatools
+```
+
+---
+
+License: MIT
---
+++ stringutils.go
@@ -0,0 +1,30 @@
+package evatools
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Join unlike strings.join, it supports any types (uses fmt.fprint underhood).
+func Join[T any](input []T, sep string) string {
+	result := strings.Builder{}
+	for i, s := range input {
+		if i > 0 {
+			result.WriteString(sep)
+		}
+		_, err := fmt.Fprint(&result, s)
+		if err != nil {
+			panic("join error")
+		}
+	}
+	return result.String()
+}
+
+// EscapeMarkdownV2 escapes string for using in telegram bots. Don't use it besides telegram bot api.
+func EscapeMarkdownV2(s string) string {
+	chars := []string{"_", "*", "[", "]", "(", ")", "~", "`", ">", "#", "+", "-", "=", "|", "{", "}", ".", "!"}
+	for _, c := range chars {
+		s = strings.Replace(s, c, `\`+c, -1)
+	}
+	return s
+}
